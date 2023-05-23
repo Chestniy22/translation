00d97a6306a965c3b5f13f47ae42a242bdf5ffae
Chestniy22 <119978648+Chestniy22@users.noreply.github.com>
2023-05-23 18:34:24 +0300

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,3 +1,21 @@
+    index = 3
+message = ''
+for code in x_train[index]:
+    word = reverse_word_index.get(code - 3, '?')
+    message += word + ' '
+message
+
+y_train[index]
+
+maxlen = 200
+
+x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
+x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
+
+x_train[3]
+
+y_train[3]
+
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
