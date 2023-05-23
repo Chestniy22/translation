6be4d1dd315036f81a36e9f53614b4ebd3aa87c4
Chestniy22 <119978648+Chestniy22@users.noreply.github.com>
2023-05-23 17:45:15 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,9 @@ data = pd.read_csv('IMDB Dataset.csv')
 max_words=10000
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_words)
 
+x_train[3]
+y_train[3]
+
 word_index = imdb.get_word_index()
 word_index
 
