f95a4597a2a4c0dfb058bf15da268b3b8e3d392f
Chestniy22 <119978648+Chestniy22@users.noreply.github.com>
2023-05-24 16:30:25 +0300

    Update learn.py

--- learn.py
+++ learn.py
@@ -4,7 +4,24 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras import utils
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import matplotlib.pyplot as plt
 
+max_words=10000
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_words)
+
+x_train[3]
+y_train[3]
+
+word_index = imdb.get_word_index()
+word_index
+
+reverse_word_index = dict()
+for key, value in word_index.items():
+    reverse_word_index[value] = key
+    
+for i in range(1, 21):
+    print(i, '->', reverse_word_index[i])
+    
 index = 3
 message = ''
 for code in x_train[index]:
@@ -20,10 +37,7 @@ x_train = pad_sequences(x_train, maxlen=maxlen, padding='post')
 x_test = pad_sequences(x_test, maxlen=maxlen, padding='post')
 
 x_train[3]
-
 y_train[3]
-
-
 
 model = Sequential()
 model.add(Dense(128, activation='relu', input_shape=(maxlen,)))
@@ -39,3 +53,6 @@ history = model.fit(x_train,
                     epochs=25,
                     batch_size=128,
                     validation_split=0.1)
+
+scores = model.evaluate(x_test, y_test, verbose=1)
+print("Доля верных ответов на тестовых данных, в процентах:", round(scores[1] * 100, 4))
