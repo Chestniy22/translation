277c4b14cd3eb76a80ca27235e0d3d34b57bbc24
Chestniy22 <119978648+Chestniy22@users.noreply.github.com>
2023-05-25 16:01:43 +0300

    Update learn.py

--- learn.py
+++ learn.py
@@ -5,6 +5,8 @@ from tensorflow.keras import utils
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from sklearn.metrics import classification_report
 
 max_words=10000
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_words)
@@ -58,3 +60,41 @@ history = model.fit(x_train,
                     batch_size=128,
                     validation_split=0.1)
 
+target_names = ['negative', 'positive']
+start_time = time.time()
+y_pred = model.predict(x_test_new)
+end_time = time.time()
+conf_matrix = classification_report(y_pred.round(),y_test_new)
+report = classification_report(y_test_new, y_pred, target_names=target_names)
+
+with open('results.txt', 'w') as f:
+    f.write('Classification report:\n{}\n\n'.format(report))
+    f.write('time on test data: {}%\n'.format(end_time))
+    
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=(maxlen,)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam', 
+              loss='binary_crossentropy', 
+              metrics=['accuracy'])
+
+history = model.fit(x_train, 
+                    y_train, 
+                    epochs=25,
+                    batch_size=128,
+                    validation_split=0.1)
+
+target_names = ['negative', 'positive']
+start_time = time.time()
+y_pred = model.predict(x_test_new)
+end_time = time.time()
+conf_matrix = classification_report(y_pred.round(),y_test_new)
+report = classification_report(y_test_new, y_pred, target_names=target_names)
+
+with open('results2.txt', 'w') as f:
+    f.write('Classification report:\n{}\n\n'.format(report))
+    f.write('time on test data: {}%\n'.format(end_time))
